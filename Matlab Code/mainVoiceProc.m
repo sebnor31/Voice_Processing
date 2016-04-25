05bd6e3759723e8c461f635c9cfbded950dbc819
Sebkhi, Nordine <sebnor31@gatech.edu>
2016-04-25 17:24:48 -0400

    Adding plotting of spectrogram

--- Matlab Code/mainVoiceProc.m
+++ Matlab Code/mainVoiceProc.m
@@ -1,13 +1,16 @@
 close all;
 
+%% Input parameters
 audioFile = 'C:\Users\nsebkhi3\Dropbox\Subject\Sub1\Hello\Hello World\Hello World_5\Hello World_5_audio1.wav';
-[audioData, fs] = audioread(audioFile);
+fontSize = 20;
+
 
 %% Get output parameters after applying downsampling and VAD
 ADR = 10;
-FrameLen = 300; %256
-StepLen = 210;  %200
+FrameLen = 300;
+StepLen = 210;
 [ startTime, stopTime, totalTime, audioOut, fsOut ] = getActiveSpeech( audioFile, ADR, FrameLen, StepLen);
+
 
 %% Plot audio waveform
 figure();
@@ -20,12 +23,28 @@ time = 0 : ( numAudioPts - 1);
 time = (1.0 / fsOut) .* time; 
 
 % Plot downsampled audio data as waveform with start and stop active speech
-plot(time, audioOut);
-plot([startTime startTime], [-1 1], 'g', 'LineWidth', 2);
-plot([stopTime stopTime], [-1 1], 'r', 'LineWidth', 2);
+plot(time, audioOut, 'b', 'LineWidth', 1);
+plot([startTime startTime], [-1 1], 'g', 'LineWidth', 5);
+plot([stopTime stopTime], [-1 1], 'r', 'LineWidth', 5);
 hold off;
 
 % Format axis
 ax = gca;
+ax.FontSize = fontSize;
 ax.Title.String = 'Audio Waveform of "Hello World"';
-ax.XLabel.String = 'Time(s)';
+% ax.XLabel.String = 'Time (s)';
+ax.XLim = [0 , 4];
+ax.YLabel.String = 'Normalized Amplitude';
+ax.YLim = [-0.5 , 0.5];
+
+%% Spectrogram
+subplot(2,1,2);
+spectrogram(audioOut, 128, 120, 128, fsOut, 'yaxis');
+colorbar off;   % Needed to align plot with waveform
+
+ax = gca;
+ax.FontSize = fontSize;
+ax.Title.String = 'Spectrogram of "Hello World"';
+ax.XLabel.String = 'Time (s)';
+ax.XLim = [0 , 4];
+
